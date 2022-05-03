e97fa275cc017ee222d169e38d3e09990275cd40
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-05-03 10:10:47 +0900

    Windows でビルドが失敗する問題を修正した。

--- virsh/src/main.rs
+++ virsh/src/main.rs
@@ -66,27 +66,8 @@ fn connect(uri: Url, readonly: bool) -> Result<Box<dyn Libvirt>, Error> {
     let schemes: Vec<&str> = uri.scheme().splitn(2, '+').collect();
 
     let mut client: Box<dyn Libvirt> = match schemes[1] {
-        "tcp" => {
-            let host = format!(
-                "{}:{}",
-                uri.host()
-                    .map(|h| h.to_string())
-                    .unwrap_or_else(|| "127.0.0.1".to_string()),
-                uri.port().unwrap_or(16509)
-            );
-            trace!("connecting: {}", &host);
-            let stream = TcpStream::connect(host)?;
-            Ok(Box::new(Client::new(stream)))
-        }
-        "unix" if cfg!(unix) => {
-            let socket = match uri.query_pairs().find(|(k, _)| k == "socket") {
-                Some((_, v)) => v.into_owned(),
-                _ => "/var/run/libvirt/libvirt-sock".to_string(),
-            };
-            trace!("connecting: {}", &socket);
-            let stream = UnixStream::connect(&socket)?;
-            Ok(Box::new(Client::new(stream)))
-        }
+        "tcp" => connect_tcp(&uri),
+        "unix" => connect_unix(&uri),
         _ => Err(Error::NotSupported),
     }?;
 
@@ -100,4 +81,33 @@ fn connect(uri: Url, readonly: bool) -> Result<Box<dyn Libvirt>, Error> {
     client.connect_open(args)?;
 
     Ok(client)
+}
+
+fn connect_tcp(uri: &Url) -> Result<Box<dyn Libvirt>, Error> {
+    let host = format!(
+        "{}:{}",
+        uri.host()
+            .map(|h| h.to_string())
+            .unwrap_or_else(|| "127.0.0.1".to_string()),
+        uri.port().unwrap_or(16509)
+    );
+    trace!("connecting: {}", &host);
+    let stream = TcpStream::connect(host)?;
+    Ok(Box::new(Client::new(stream)))
+}
+
+#[cfg(target_family = "unix")]
+fn connect_unix(uri: &Url) -> Result<Box<dyn Libvirt>, Error> {
+    let socket = match uri.query_pairs().find(|(k, _)| k == "socket") {
+        Some((_, v)) => v.into_owned(),
+        _ => "/var/run/libvirt/libvirt-sock".to_string(),
+    };
+    trace!("connecting: {}", &socket);
+    let stream = UnixStream::connect(&socket)?;
+    Ok(Box::new(Client::new(stream)))
+}
+
+#[cfg(target_family = "windows")]
+fn connect_unix(_: &Url) -> Result<Box<dyn Libvirt>, Error> {
+    Err(Error::NotSupported)
 }
