d51be9b5235c4d5a110def60d4ee3ea48046fe46
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-09-02 20:49:11 +0900

    コード生成ツールのバージョンを更新した。

--- libvirt-remote/tools/gen.sh
+++ libvirt-remote/tools/gen.sh
@@ -3,7 +3,7 @@
 set -eu
 
 VERSION="v8.6.0"
-TOOL_VERSION="0.3.0"
+TOOL_VERSION="0.4.0"
 PROTO_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/rpc/virnetprotocol.x"
 REMOTE_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/remote_protocol.x"
 RPCGEN_BIN="https://github.com/9506hqwy/xdr-rs/releases/download/${TOOL_VERSION}/rpcgen-${TOOL_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
