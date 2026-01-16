a4746c66b032e13df665e902b4efa8766cfa164d
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-01-17 00:18:36 +0900

    自動生成ファイルを v12.0.0 に更新した。

--- libvirt-remote/tools/gen.sh
+++ libvirt-remote/tools/gen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-VERSION="v11.10.0"
+VERSION="v12.0.0"
 TOOL_VERSION="0.4.0"
 PROTO_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/rpc/virnetprotocol.x"
 REMOTE_URL="https://raw.githubusercontent.com/libvirt/libvirt/${VERSION}/src/remote/remote_protocol.x"
